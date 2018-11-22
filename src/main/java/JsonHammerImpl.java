7a6c0c5419d96cdecd5373e3d3b4f3eb7d28f58f
wujundi <wujundimail@163.com>
2018-11-22 23:37:59 +0800

    2018-11-22

--- src/main/java/JsonHammerImpl.java
+++ src/main/java/JsonHammerImpl.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class JsonHammerImpl implements JsonHammer{
 
+    /*
+    思路小记
+    josn-hammer 的本质是可能存在多层嵌套的数组完全打横，
+    那么可以理解成将各个数组并排放在一起，取其中元素组合的笛卡尔积
+    [][][][][]
+    ()()()
+    <><><><>
+    所以，将所有 jsonObject 一字排开
+    将所有 jsonArray 各自拆成List
+    然后用一个多层嵌套的循环来完成就可以了（当然在实现上可以采用二位数组，不一定非得是多层循环）
+     */
+
 
     public List<String> hammer(String json_str) {
         if(json_str == null || json_str.length() <= 1){
